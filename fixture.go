2049fd3167be1a30bff3744cf07967f3165af061
Iordanis Paschalidis <iordanispaschalidis@Iordaniss-Mac-mini.local>
2023-10-04 14:17:44 +0100

    more endpoints

--- fixture.go
+++ fixture.go
@@ -2,7 +2,10 @@ package client
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/0ffsideCompass/api-football-go-client/models"
@@ -12,13 +15,95 @@ const (
 	fixturesByDateEndpoint    = "fixtures?league=%d&season=%d&from=%s&to=%s"
 	fixtureHeadToHeadEndpoint = "fixtures/headtohead?h2h=%d-%d"
 	fixtureRounds             = "fixtures/rounds?league=%d&season=%d" //TODO not important yet
-	fixture                   = "fixtures"
+	fixtureEndpoint           = "fixtures"
+	fixtureStatisticsEndpoint = "fixtures/statistics"
+	fixturesEventsEndpoint    = "fixtures/events"
+	fixtureLineupsEndpoint    = "fixtures/lineups"
+	fixturePlayerEndpoint     = "fixtures/players"
 )
+
+// FixturesLineups returns lineups for a given fixture
+/*
+	- fixture: (Type: integer)(Required)
+  	The ID of the fixture. Value format: 43.
+	- team: (Type: integer)
+	The ID of the team. Value format: 85.
+	- player: (Type: integer)
+	The ID of the player. Value format: 85.
+*/
+func (c *Client) FixturesLineups(
+	params map[string]interface{},
+) (*models.FixturesLineupsResponse, error) {
+
+	// Validate the parameters
+	if err := validateFixturesEventsLineupsParams(params); err != nil {
+		return nil, err
+	}
+
+	endpointURL := fmt.Sprintf("%s%s", c.Domain, fixturesEventsEndpoint)
+	body, err := c.get(
+		c.buildURL(
+			endpointURL,
+			params,
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error getting fixtures events: %w", err)
+	}
+
+	var resp models.FixturesLineupsResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"error unmarshalling fixtures lineups response: %w",
+			err,
+		)
+	}
+	return &resp, nil
+}
+
+// FixturesEvents returns all events for a given fixture
+/*
+	- fixture: (Type: integer)(Required)
+  	The ID of the fixture. Value format: 43.
+	- team: (Type: integer)
+	The ID of the team. Value format: 85.
+	- player: (Type: integer)
+	The ID of the player. Value format: 85.
+*/
+func (c *Client) FixturesEvents(
+	params map[string]interface{},
+) (*models.FixturesEventsResponse, error) {
+
+	// Validate the parameters
+	if err := validateFixturesEventsLineupsParams(params); err != nil {
+		return nil, err
+	}
+
+	endpointURL := fmt.Sprintf("%s%s", c.Domain, fixturesEventsEndpoint)
+	body, err := c.get(
+		c.buildURL(
+			endpointURL,
+			params,
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error getting fixtures events: %w", err)
+	}
+
+	var resp models.FixturesEventsResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"error unmarshalling fixtures events response: %w",
+			err,
+		)
+	}
+	return &resp, nil
+}
 
 // Fixture returns all fixtures for parameters passed
 /*
-============================== QUERY PARAMETERS ==============================
-
 - id: (Type: integer)
   The ID of the fixture. Value format: 43.
 
@@ -37,16 +122,16 @@ const (
 - league: (Type: integer)
   The ID of the league.
 
-- season: (Type: integer)
+- season: (Type: integer) (should be 4 digits)
   The season of the league. Format: 4 characters YYYY.
 
 - team: (Type: integer)
   The ID of the team.
 
-- last: (Type: integer)
+- last: (Type: integer) (<= 2 digis)
   For the X last fixtures. Value should be an integer with <= 2 characters.
 
-- next: (Type: integer)
+- next: (Type: integer) (<= 2 digis)
   For the X next fixtures. Value should be an integer with <= 2 characters.
 
 - from: (Type: string)
@@ -70,8 +155,14 @@ const (
 */
 func (c *Client) Fixture(
 	params map[string]interface{},
-) (*models.FixturesByDateResp, error) {
-	endpointURL := fmt.Sprintf("%s/%s", c.Domain, fixture)
+) (*models.FixturesResponse, error) {
+
+	// Validate the parameters
+	if err := validateFixtureParams(params); err != nil {
+		return nil, err
+	}
+
+	endpointURL := fmt.Sprintf("%s/%s", c.Domain, fixtureEndpoint)
 
 	body, err := c.get(
 		c.buildURL(
@@ -82,14 +173,67 @@ func (c *Client) Fixture(
 	if err != nil {
 		return nil, fmt.Errorf("error getting fixtures: %w", err)
 	}
-	fmt.Println(string(body))
-	var resp models.FixturesByDateResp
+	var resp models.FixturesResponse
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
 		return nil, fmt.Errorf(
 			"error unmarshalling fixtures response: %w",
 			err,
 		)
+	}
+	return &resp, nil
+}
+
+// FixtureHeadToHead returns head to head for two teams
+/*
+	- h2h: (Type: string)(Required)(format id-id)
+	The ids of the teams
+	- date (Type: string)(format YYYY-MM-DD)
+	The date of the match
+	- league (Type: integer)
+	The id of the league
+	- season (Type: integer)(4 digits)(format YYYY)
+	The season of the league
+	- last (Type: integer)
+	For the X last fixtures.
+	- next (Type: integer)
+	For the X next fixtures.
+	- from (Type: string)(format YYYY-MM-DD)
+	A valid starting date for a date range.
+	- to (Type: string)(format YYYY-MM-DD)
+	A valid ending date for a date range.
+	- venue (Type: integer)
+	The venue ID of the fixture.
+	- status (Type: string)
+	Enumerated values:
+	* "NS": A specific fixture status.
+	* "NS-PST-FT": Combined fixture statuses.
+*/
+func (c *Client) FixtureHeadToHead(
+	params map[string]interface{},
+) (*models.FixtureHeadToHeadResp, error) {
+
+	// Validate the parameters
+	if err := validateFixtureHeadToHeadParams(params); err != nil {
+		return nil, err
+	}
+
+	endpointURL := fmt.Sprintf("%s%s", c.Domain, trophiesEndpoint)
+
+	body, err := c.get(
+		fmt.Sprintf(
+			endpointURL,
+			params,
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp models.FixtureHeadToHeadResp
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, err
 	}
 	return &resp, nil
 }
@@ -125,25 +269,245 @@ func (c *Client) FixtureByDateAndLeague(
 	return &resp, nil
 }
 
-func (c *Client) FixtureHeadToHead(
-	teamOneID,
-	teamTwoID int,
-) (*models.FixtureHeadToHeadResp, error) {
-	body, err := c.get(
-		fmt.Sprintf(
-			fixtureHeadToHeadEndpoint,
-			teamOneID,
-			teamTwoID,
-		),
-	)
-	if err != nil {
+// FixtureStatistics returns statistics for a given fixture
+/*
+	- fixture (Type: integer)(Required)
+	The ID of the fixture. Value format: 43.
+	- team (Type: integer)
+	The ID of the team. Value format: 85.
+*/
+func (c *Client) FixtureStatistics(
+	params map[string]interface{},
+) (*models.FixturesStatisticsResponse, error) {
+
+	// Validate the parameters
+	if err := validateFixtureStatisticsPlayerParams(params); err != nil {
 		return nil, err
 	}
 
-	var resp models.FixtureHeadToHeadResp
+	endpointURL := fmt.Sprintf("%s%s", c.Domain, fixtureStatisticsEndpoint)
+	body, err := c.get(
+		c.buildURL(
+			endpointURL,
+			params,
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error getting fixtures statistics: %w", err)
+	}
+
+	var resp models.FixturesStatisticsResponse
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf(
+			"error unmarshalling fixtures statistics response: %w",
+			err,
+		)
 	}
 	return &resp, nil
+}
+
+// FixturesPlayer returns fixture's player data
+/*
+	- fixture (Type: integer)(Required)
+	The ID of the fixture. Value format: 43.
+	- team (Type: integer)
+	The ID of the team. Value format: 85.
+*/
+func (c *Client) FixturesPlayer(
+	params map[string]interface{},
+) (*models.FixturesPlayersResponse, error) {
+	// Validate the parameters
+	if err := validateFixtureStatisticsPlayerParams(params); err != nil {
+		return nil, err
+	}
+
+	endpointURL := fmt.Sprintf("%s%s", c.Domain, fixturePlayerEndpoint)
+	body, err := c.get(
+		c.buildURL(
+			endpointURL,
+			params,
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error getting fixtures statistics: %w", err)
+	}
+
+	var resp models.FixturesPlayersResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"error unmarshalling fixtures player response: %w",
+			err,
+		)
+	}
+	return &resp, nil
+}
+
+func validateFixtureParams(params map[string]interface{}) error {
+
+	// Validate ID
+	if id, exists := params["id"].(int); exists && id < 1 {
+		return errors.New("invalid id parameter")
+	}
+
+	// Validate IDs
+	if ids, exists := params["ids"].(string); exists {
+		idList := strings.Split(ids, "-")
+		if len(idList) > 20 {
+			return errors.New("maximum of 20 fixture IDs allowed")
+		}
+		for _, id := range idList {
+			if _, err := strconv.Atoi(id); err != nil {
+				return errors.New("invalid id in ids parameter")
+			}
+		}
+	}
+
+	// Validate date
+	if date, exists := params["date"].(string); exists {
+		_, err := time.Parse("2006-01-02", date)
+		if err != nil {
+			return errors.New("invalid date format")
+		}
+	}
+
+	// Validate from and to
+	if fromDate, exists := params["from"].(string); exists {
+		_, err := time.Parse("2006-01-02", fromDate)
+		if err != nil {
+			return errors.New("invalid from date format")
+		}
+	}
+	if toDate, exists := params["to"].(string); exists {
+		_, err := time.Parse("2006-01-02", toDate)
+		if err != nil {
+			return errors.New("invalid to date format")
+		}
+	}
+
+	// Validate season
+	if season, exists := params["season"].(int); exists && (season < 1000 || season > 9999) {
+		return errors.New("invalid season format. Season should be 4 digits")
+	}
+
+	// Validate last and next
+	if last, exists := params["last"].(int); exists && (last < 0 || last > 99) {
+		return errors.New("invalid last parameter. Value should be an integer with <= 2 characters")
+	}
+	if next, exists := params["next"].(int); exists && (next < 0 || next > 99) {
+		return errors.New("invalid next parameter. Value should be an integer with <= 2 characters")
+	}
+
+	// Validate status
+	if status, exists := params["status"].(string); exists {
+		statusList := strings.Split(status, "-")
+		validStatuses := map[string]bool{
+			"NS":  true,
+			"PST": true,
+			"FT":  true,
+		}
+		for _, s := range statusList {
+			if _, valid := validStatuses[s]; !valid {
+				return errors.New("invalid status value")
+			}
+		}
+	}
+
+	return nil
+}
+
+func validateFixtureHeadToHeadParams(params map[string]interface{}) error {
+
+	// Validate h2h (head-to-head)
+	if h2h, exists := params["h2h"].(string); exists {
+		teamIds := strings.Split(h2h, "-")
+		if len(teamIds) != 2 {
+			return errors.New("h2h parameter must have exactly two team IDs separated by '-'")
+		}
+		for _, id := range teamIds {
+			if _, err := strconv.Atoi(id); err != nil {
+				return errors.New("invalid team ID in h2h parameter")
+			}
+		}
+	} else {
+		return errors.New("h2h parameter is required")
+	}
+
+	// Validate date
+	if date, exists := params["date"].(string); exists {
+		_, err := time.Parse("2006-01-02", date)
+		if err != nil {
+			return errors.New("invalid date format")
+		}
+	}
+
+	// Validate from and to
+	if fromDate, exists := params["from"].(string); exists {
+		_, err := time.Parse("2006-01-02", fromDate)
+		if err != nil {
+			return errors.New("invalid from date format")
+		}
+	}
+	if toDate, exists := params["to"].(string); exists {
+		_, err := time.Parse("2006-01-02", toDate)
+		if err != nil {
+			return errors.New("invalid to date format")
+		}
+	}
+
+	// Validate season
+	if season, exists := params["season"].(int); exists && (season < 1000 || season > 9999) {
+		return errors.New("invalid season format. Season should be 4 digits")
+	}
+
+	// Validate status
+	if status, exists := params["status"].(string); exists {
+		statusList := strings.Split(status, "-")
+		validStatuses := map[string]bool{
+			"NS":  true,
+			"PST": true,
+			"FT":  true,
+		}
+		for _, s := range statusList {
+			if _, valid := validStatuses[s]; !valid {
+				return errors.New("invalid status value")
+			}
+		}
+	}
+
+	return nil
+}
+
+func validateFixtureStatisticsPlayerParams(params map[string]interface{}) error {
+	// Validate fixture
+	if fixture, exists := params["fixture"].(int); !exists || fixture < 1 {
+		return errors.New("invalid or missing fixture parameter")
+	}
+
+	// Validate team (optional, so just validate if exists)
+	if team, exists := params["team"].(int); exists && team < 1 {
+		return errors.New("invalid team parameter")
+	}
+
+	return nil
+}
+
+func validateFixturesEventsLineupsParams(params map[string]interface{}) error {
+	// Validate fixture
+	if fixture, exists := params["fixture"].(int); !exists || fixture < 1 {
+		return errors.New("invalid or missing fixture parameter")
+	}
+
+	// Validate team (optional, so just validate if exists)
+	if team, exists := params["team"].(int); exists && team < 1 {
+		return errors.New("invalid team parameter")
+	}
+
+	// Validate player (optional, so just validate if exists)
+	if player, exists := params["player"].(int); exists && player < 1 {
+		return errors.New("invalid player parameter")
+	}
+
+	return nil
 }
---
+++ models/fixtures_model.go
@@ -0,0 +1,266 @@
+package models
+
+import "time"
+
+// FixturesStatisticsResponse is the response from the /fixtures/statistics endpoint
+type FixturesStatisticsResponse struct {
+	Get        string      `json:"get"`
+	Parameters interface{} `json:"parameters"`
+	Errors     interface{} `json:"errors"`
+	Results    int         `json:"results"`
+	Paging     struct {
+		Current int `json:"current"`
+		Total   int `json:"total"`
+	} `json:"paging"`
+	Response []struct {
+		Team struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+			Logo string `json:"logo"`
+		} `json:"team"`
+		Statistics []struct {
+			Type  string `json:"type"`
+			Value int    `json:"value"`
+		} `json:"statistics"`
+	} `json:"response"`
+}
+
+// FixturesResponse is the response from the /fixtures endpoint
+type FixturesResponse struct {
+	Get        string      `json:"get"`
+	Parameters interface{} `json:"parameters"`
+	Errors     interface{} `json:"errors"`
+	Results    int         `json:"results"`
+	Paging     struct {
+		Current int `json:"current"`
+		Total   int `json:"total"`
+	} `json:"paging"`
+	Response []struct {
+		Fixture struct {
+			ID        int       `json:"id"`
+			Referee   string    `json:"referee"`
+			Timezone  string    `json:"timezone"`
+			Date      time.Time `json:"date"`
+			Timestamp int       `json:"timestamp"`
+			Periods   struct {
+				First  int `json:"first"`
+				Second int `json:"second"`
+			} `json:"periods"`
+			Venue struct {
+				ID   int    `json:"id"`
+				Name string `json:"name"`
+				City string `json:"city"`
+			} `json:"venue"`
+			Status struct {
+				Long    string `json:"long"`
+				Short   string `json:"short"`
+				Elapsed int    `json:"elapsed"`
+			} `json:"status"`
+		} `json:"fixture"`
+		League struct {
+			ID      int    `json:"id"`
+			Name    string `json:"name"`
+			Country string `json:"country"`
+			Logo    string `json:"logo"`
+			Flag    string `json:"flag"`
+			Season  int    `json:"season"`
+			Round   string `json:"round"`
+		} `json:"league"`
+		Teams struct {
+			Home struct {
+				ID     int         `json:"id"`
+				Name   string      `json:"name"`
+				Logo   string      `json:"logo"`
+				Winner interface{} `json:"winner"`
+			} `json:"home"`
+			Away struct {
+				ID     int         `json:"id"`
+				Name   string      `json:"name"`
+				Logo   string      `json:"logo"`
+				Winner interface{} `json:"winner"`
+			} `json:"away"`
+		} `json:"teams"`
+		Goals struct {
+			Home int `json:"home"`
+			Away int `json:"away"`
+		} `json:"goals"`
+		Score struct {
+			Halftime struct {
+				Home int `json:"home"`
+				Away int `json:"away"`
+			} `json:"halftime"`
+			Fulltime struct {
+				Home int `json:"home"`
+				Away int `json:"away"`
+			} `json:"fulltime"`
+			Extratime struct {
+				Home interface{} `json:"home"`
+				Away interface{} `json:"away"`
+			} `json:"extratime"`
+			Penalty struct {
+				Home interface{} `json:"home"`
+				Away interface{} `json:"away"`
+			} `json:"penalty"`
+		} `json:"score"`
+	} `json:"response"`
+}
+
+// FixturesEventsResponse is the response from the /fixtures/events endpoint
+type FixturesEventsResponse struct {
+	Get        string      `json:"get"`
+	Parameters interface{} `json:"parameters"`
+	Errors     interface{} `json:"errors"`
+	Results    int         `json:"results"`
+	Paging     struct {
+		Current int `json:"current"`
+		Total   int `json:"total"`
+	} `json:"paging"`
+	Response []struct {
+		Time struct {
+			Elapsed int         `json:"elapsed"`
+			Extra   interface{} `json:"extra"`
+		} `json:"time"`
+		Team struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+			Logo string `json:"logo"`
+		} `json:"team"`
+		Player struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+		} `json:"player"`
+		Assist struct {
+			ID   interface{} `json:"id"`
+			Name interface{} `json:"name"`
+		} `json:"assist"`
+		Type     string      `json:"type"`
+		Detail   string      `json:"detail"`
+		Comments interface{} `json:"comments"`
+	} `json:"response"`
+}
+
+// FixturesLineupsResponse is the response from the /fixtures/lineups endpoint
+type FixturesLineupsResponse struct {
+	Get        string      `json:"get"`
+	Parameters interface{} `json:"parameters"`
+	Errors     interface{} `json:"errors"`
+	Results    int         `json:"results"`
+	Paging     struct {
+		Current int `json:"current"`
+		Total   int `json:"total"`
+	} `json:"paging"`
+	Response []struct {
+		Team struct {
+			ID     int         `json:"id"`
+			Name   string      `json:"name"`
+			Logo   string      `json:"logo"`
+			Colors interface{} `json:"colors"`
+		} `json:"team"`
+		Coach struct {
+			ID    int    `json:"id"`
+			Name  string `json:"name"`
+			Photo string `json:"photo"`
+		} `json:"coach"`
+		Formation string `json:"formation"`
+		StartXI   []struct {
+			Player struct {
+				ID     int    `json:"id"`
+				Name   string `json:"name"`
+				Number int    `json:"number"`
+				Pos    string `json:"pos"`
+				Grid   string `json:"grid"`
+			} `json:"player"`
+		} `json:"startXI"`
+		Substitutes []struct {
+			Player struct {
+				ID     int         `json:"id"`
+				Name   string      `json:"name"`
+				Number int         `json:"number"`
+				Pos    string      `json:"pos"`
+				Grid   interface{} `json:"grid"`
+			} `json:"player"`
+		} `json:"substitutes"`
+	} `json:"response"`
+}
+
+// FixturesPlayersResponse is the response from the /fixtures/players endpoint
+type FixturesPlayersResponse struct {
+	Get        string      `json:"get"`
+	Parameters interface{} `json:"parameters"`
+	Errors     interface{} `json:"errors"`
+	Results    int         `json:"results"`
+	Paging     struct {
+		Current int `json:"current"`
+		Total   int `json:"total"`
+	} `json:"paging"`
+	Response []struct {
+		Team struct {
+			ID     int       `json:"id"`
+			Name   string    `json:"name"`
+			Logo   string    `json:"logo"`
+			Update time.Time `json:"update"`
+		} `json:"team"`
+		Players []struct {
+			Player struct {
+				ID    int    `json:"id"`
+				Name  string `json:"name"`
+				Photo string `json:"photo"`
+			} `json:"player"`
+			Statistics []struct {
+				Games struct {
+					Minutes    int    `json:"minutes"`
+					Number     int    `json:"number"`
+					Position   string `json:"position"`
+					Rating     string `json:"rating"`
+					Captain    bool   `json:"captain"`
+					Substitute bool   `json:"substitute"`
+				} `json:"games"`
+				Offsides interface{} `json:"offsides"`
+				Shots    struct {
+					Total int `json:"total"`
+					On    int `json:"on"`
+				} `json:"shots"`
+				Goals struct {
+					Total    interface{} `json:"total"`
+					Conceded int         `json:"conceded"`
+					Assists  interface{} `json:"assists"`
+					Saves    int         `json:"saves"`
+				} `json:"goals"`
+				Passes struct {
+					Total    int    `json:"total"`
+					Key      int    `json:"key"`
+					Accuracy string `json:"accuracy"`
+				} `json:"passes"`
+				Tackles struct {
+					Total         interface{} `json:"total"`
+					Blocks        int         `json:"blocks"`
+					Interceptions int         `json:"interceptions"`
+				} `json:"tackles"`
+				Duels struct {
+					Total interface{} `json:"total"`
+					Won   interface{} `json:"won"`
+				} `json:"duels"`
+				Dribbles struct {
+					Attempts int         `json:"attempts"`
+					Success  int         `json:"success"`
+					Past     interface{} `json:"past"`
+				} `json:"dribbles"`
+				Fouls struct {
+					Drawn     int `json:"drawn"`
+					Committed int `json:"committed"`
+				} `json:"fouls"`
+				Cards struct {
+					Yellow int `json:"yellow"`
+					Red    int `json:"red"`
+				} `json:"cards"`
+				Penalty struct {
+					Won      interface{} `json:"won"`
+					Commited interface{} `json:"commited"`
+					Scored   int         `json:"scored"`
+					Missed   int         `json:"missed"`
+					Saved    int         `json:"saved"`
+				} `json:"penalty"`
+			} `json:"statistics"`
+		} `json:"players"`
+	} `json:"response"`
+}
