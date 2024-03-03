8893b0c1312231cd1bd1ecfb83671b3327b43eff
Iordanis Paschalidis <iordanispaschalidis@Iordaniss-Mac-mini.local>
2024-03-03 13:21:50 +0000

    adding new stuff boys

--- models/fixtures_model.go
+++ models/fixtures_model.go
@@ -102,6 +102,152 @@ type FixturesResponse struct {
 				Away interface{} `json:"away"`
 			} `json:"penalty"`
 		} `json:"score"`
+		Lineups []struct {
+			Team struct {
+				ID     int    `json:"id"`
+				Name   string `json:"name"`
+				Logo   string `json:"logo"`
+				Colors struct {
+					Player struct {
+						Primary string `json:"primary"`
+						Number  string `json:"number"`
+						Border  string `json:"border"`
+					} `json:"player"`
+					Goalkeeper struct {
+						Primary string `json:"primary"`
+						Number  string `json:"number"`
+						Border  string `json:"border"`
+					} `json:"goalkeeper"`
+				} `json:"colors"`
+			} `json:"team"`
+			Formation string `json:"formation"`
+			StartXI   []struct {
+				Player struct {
+					ID     int    `json:"id"`
+					Name   string `json:"name"`
+					Number int    `json:"number"`
+					Pos    string `json:"pos"`
+					Grid   string `json:"grid"`
+				} `json:"player"`
+			} `json:"startXI"`
+			Substitutes []struct {
+				Player struct {
+					ID     int         `json:"id"`
+					Name   string      `json:"name"`
+					Number int         `json:"number"`
+					Pos    string      `json:"pos"`
+					Grid   interface{} `json:"grid"`
+				} `json:"player"`
+			} `json:"substitutes"`
+			Coach struct {
+				ID    int    `json:"id"`
+				Name  string `json:"name"`
+				Photo string `json:"photo"`
+			} `json:"coach"`
+		} `json:"lineups"`
+		Statistics []struct {
+			Team struct {
+				ID   int    `json:"id"`
+				Name string `json:"name"`
+				Logo string `json:"logo"`
+			} `json:"team"`
+			Statistics []struct {
+				Type  string `json:"type"`
+				Value int    `json:"value"`
+			} `json:"statistics"`
+		} `json:"statistics"`
+		Players []struct {
+			Team struct {
+				ID     int       `json:"id"`
+				Name   string    `json:"name"`
+				Logo   string    `json:"logo"`
+				Update time.Time `json:"update"`
+			} `json:"team"`
+			Players []struct {
+				Player struct {
+					ID    int    `json:"id"`
+					Name  string `json:"name"`
+					Photo string `json:"photo"`
+				} `json:"player"`
+				Statistics []struct {
+					Games struct {
+						Minutes    int    `json:"minutes"`
+						Number     int    `json:"number"`
+						Position   string `json:"position"`
+						Rating     string `json:"rating"`
+						Captain    bool   `json:"captain"`
+						Substitute bool   `json:"substitute"`
+					} `json:"games"`
+					Offsides interface{} `json:"offsides"`
+					Shots    struct {
+						Total interface{} `json:"total"`
+						On    interface{} `json:"on"`
+					} `json:"shots"`
+					Goals struct {
+						Total    interface{} `json:"total"`
+						Conceded int         `json:"conceded"`
+						Assists  interface{} `json:"assists"`
+						Saves    int         `json:"saves"`
+					} `json:"goals"`
+					Passes struct {
+						Total    int         `json:"total"`
+						Key      interface{} `json:"key"`
+						Accuracy string      `json:"accuracy"`
+					} `json:"passes"`
+					Tackles struct {
+						Total         interface{} `json:"total"`
+						Blocks        interface{} `json:"blocks"`
+						Interceptions interface{} `json:"interceptions"`
+					} `json:"tackles"`
+					Duels struct {
+						Total interface{} `json:"total"`
+						Won   interface{} `json:"won"`
+					} `json:"duels"`
+					Dribbles struct {
+						Attempts interface{} `json:"attempts"`
+						Success  interface{} `json:"success"`
+						Past     interface{} `json:"past"`
+					} `json:"dribbles"`
+					Fouls struct {
+						Drawn     interface{} `json:"drawn"`
+						Committed interface{} `json:"committed"`
+					} `json:"fouls"`
+					Cards struct {
+						Yellow int `json:"yellow"`
+						Red    int `json:"red"`
+					} `json:"cards"`
+					Penalty struct {
+						Won      interface{} `json:"won"`
+						Commited interface{} `json:"commited"`
+						Scored   int         `json:"scored"`
+						Missed   int         `json:"missed"`
+						Saved    int         `json:"saved"`
+					} `json:"penalty"`
+				} `json:"statistics"`
+			} `json:"players"`
+		} `json:"players"`
+		Events []struct {
+			Time struct {
+				Elapsed int         `json:"elapsed"`
+				Extra   interface{} `json:"extra"`
+			} `json:"time"`
+			Team struct {
+				ID   int    `json:"id"`
+				Name string `json:"name"`
+				Logo string `json:"logo"`
+			} `json:"team"`
+			Player struct {
+				ID   int    `json:"id"`
+				Name string `json:"name"`
+			} `json:"player"`
+			Assist struct {
+				ID   interface{} `json:"id"`
+				Name interface{} `json:"name"`
+			} `json:"assist"`
+			Type     string `json:"type"`
+			Detail   string `json:"detail"`
+			Comments string `json:"comments"`
+		} `json:"events"`
 	} `json:"response"`
 }
 
