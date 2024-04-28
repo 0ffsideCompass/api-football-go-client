3aa765fb1f6072013f19d41491f0abf2dc95c8b6
Iordanis Paschalidis <iordanispaschalidis@Iordaniss-Mac-mini.local>
2024-04-28 19:17:58 +0100

    removing validation

--- standings.go
+++ standings.go
@@ -2,9 +2,7 @@ package client
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/0ffsideCompass/api-football-go-client/models"
 )
@@ -25,12 +23,6 @@ const (
 func (c *Client) Standings(
 	params map[string]interface{},
 ) (*models.StandingsResponse, error) {
-
-	err := c.standingsValidations(params)
-	if err != nil {
-		return nil, fmt.Errorf("error validating standings params: %w", err)
-	}
-
 	endpointURL := fmt.Sprintf("%s%s", c.Domain, standingsEndpoint)
 	body, err := c.get(
 		c.buildURL(
@@ -50,34 +42,4 @@ func (c *Client) Standings(
 		)
 	}
 	return &resp, nil
-}
-
-func (c *Client) standingsValidations(
-	params map[string]interface{},
-) error {
-	// Check if season is provided in params
-	season, seasonExists := params["season"]
-	if !seasonExists {
-		return errors.New("season is required in the arguments")
-	}
-
-	// Check if season is provided in params
-	_, leagueExists := params["league"]
-	if !leagueExists {
-		return errors.New("league is required in the arguments")
-	}
-
-	// Ensure that season is a valid integer
-	seasonInt, ok := season.(int)
-	if !ok {
-		return errors.New("season must be an integer")
-	}
-
-	// Check if season is exactly 4 digits
-	seasonStr := strconv.Itoa(seasonInt)
-	if len(seasonStr) != 4 {
-		return errors.New("season must be a 4-digit integer")
-	}
-
-	return nil
 }
